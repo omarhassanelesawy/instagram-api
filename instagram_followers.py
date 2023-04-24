98affcf84fa50b050019b18a7420d658b34ee1b3
omarhaassaan <omarhassan321@hotmail.com>
2023-04-24 22:14:54 +0200

    V1 of the instagram code for following not followers

---
+++ instagram_followers.py
@@ -0,0 +1,16 @@
+from instagrapi import Client
+# Credentials
+username = ''
+password = ''
+# Create Client for your instagram account
+client = Client()
+client.login(username=username, password=password)
+user_id = client.user_id_from_username(password)
+# Get Followers
+followers = client.user_followers(user_id)
+# Get Followings
+followings = client.user_following(user_id)
+# Check and print any following username that isn't in the followers
+for following in followings:
+    if not followings[following].username in [followers[follower].username for follower in followers]:
+        print(followings[following].username)
