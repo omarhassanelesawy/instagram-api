2fc08277fec187ee2da8a0b4b4141eb53a167b60
Elesawy <omar.elesawy.ext@siemens.com>
2024-09-26 12:52:23 +0300

    Fix Maintainability issue

--- instagram_followers.py
+++ instagram_followers.py
@@ -12,5 +12,5 @@ followers = client.user_followers(user_id)
 followings = client.user_following(user_id)
 # Check and print any following username that isn't in the followers
 for following in followings:
-    if not followings[following].username in [followers[follower].username for follower in followers]:
+    if followings[following].username not in [followers[follower].username for follower in followers]:
         print(followings[following].username)
